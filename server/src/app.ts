45d9b58c57ef94aa97cad7aa8dce13cfd2c669f6
Giancyril <mijaresgiancyril@gmail.com>
2026-03-15 17:57:49 +0800

    fix: add Cache-Control and Pragma to CORS allowed headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,13 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "authorization",
+      "Cache-Control",
+      "Pragma",
+    ],
   })
 );
 
