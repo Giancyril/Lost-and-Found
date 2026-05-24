2ace5a7baea294942c76662828f06461b50bd96e
Giancyril <mijaresgiancyril@gmail.com>
2026-05-24 11:12:10 +0800

    chore: remove fake security detectors

--- frontend/src/providers/Providers.tsx
+++ frontend/src/providers/Providers.tsx
@@ -1,15 +1,11 @@
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { StudentProvider } from "../components/context/StudentContext";
-import DevToolsDetector from "../components/DevToolsDetector";
-import SecurityHoneypot from "../components/SecurityHoneypot";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <StudentProvider>
-        <DevToolsDetector />
-        <SecurityHoneypot />
         {children}
       </StudentProvider>
     </Provider>
