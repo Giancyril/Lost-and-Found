3db37f197f50cd8c67e30d66661690babdda330c
Giancyril <mijaresgiancyril@gmail.com>
2026-05-24 11:14:16 +0800

    feat: restore security honeypot

--- frontend/src/providers/Providers.tsx
+++ frontend/src/providers/Providers.tsx
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import { StudentProvider } from "../components/context/StudentContext";
+import SecurityHoneypot from "../components/SecurityHoneypot";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <StudentProvider>
+        <SecurityHoneypot />
         {children}
       </StudentProvider>
     </Provider>
